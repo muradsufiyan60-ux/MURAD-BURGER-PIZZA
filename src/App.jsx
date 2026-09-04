89cd8627e27c54ca1cf46b1cb19e38aa4cedee26
Murad Sufiyan <muradsufiyan60@gmail.com>
2026-09-04 12:08:03 +0300

    Update basename to MURAD-BURGER-PIZZA

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <Router basename="/barger-project">
+        <Router basename="/MURAD-BURGER-PIZZA">
           <div className="min-h-screen flex flex-col justify-between bg-[#FAF9F6]">
             <Navbar />
             <Routes>
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/barger-project/', // Matches repository name for GitHub Pages
+  base: '/MURAD-BURGER-PIZZA/',
   plugins: [
     react(),
     tailwindcss(),
